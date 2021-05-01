423fb2142aa3d4d2d6e4412f9de75aab2c3915f0
Jakub <jakub@cubepotato.eu>
2021-05-01 13:02:06 +0200

    cpu usage

--- src/web.rs
+++ src/web.rs
@@ -10,6 +10,7 @@ use serenity::http::CacheHttp;
 use std::io::Cursor;
 use sysinfo::{ProcessExt, SystemExt};
 use systemstat::{ByteSize, Platform, System};
+use tokio::time::{sleep, Duration};
 
 impl<'a> Responder<'a, 'a> for Error {
     fn respond_to(self, _: &'a Request<'_>) -> response::Result<'a> {
@@ -21,23 +22,32 @@ impl<'a> Responder<'a, 'a> for Error {
     }
 }
 
-fn get_proc_mem() -> Result<String, Error> {
+fn get_proc_stats() -> Result<String, Error> {
     let pid = sysinfo::get_current_pid()?;
     let sys = sysinfo::System::new_all();
     let proc = sys
         .get_process(pid)
         .ok_or(Error::Custom("process not found"))?;
 
-    Ok(format!("{}", ByteSize::kb(proc.memory()),))
+    Ok(format!(
+        "{}, cpu {}%",
+        ByteSize::kb(proc.memory()),
+        proc.cpu_usage()
+    ))
 }
 
-fn get_sys_mem() -> Result<String, Error> {
+async fn get_sys_stats() -> Result<String, Error> {
     let sys = System::new();
     let mem = sys.memory()?;
+    let cpu_delayed = sys.cpu_load_aggregate()?;
+    sleep(Duration::from_millis(1000)).await;
+    let cpu = cpu_delayed.done()?;
+
     Ok(format!(
-        "{}/{}",
+        "{}/{}, cpu {}%",
         systemstat::saturating_sub_bytes(mem.total, mem.free),
         mem.total,
+        cpu.user * 100f32,
     ))
 }
 
@@ -50,18 +60,18 @@ async fn hello(cache_http: State<'_, Box<dyn CacheHttp>>) -> Result<String, Erro
         .await
         .tag();
 
-    let proc_mem = match get_proc_mem() {
+    let proc_mem = match get_proc_stats() {
         Ok(m) => m,
         Err(why) => format!("<error {:?}>", why),
     };
 
-    let sys_mem = match get_sys_mem() {
+    let sys_mem = match get_sys_stats().await {
         Ok(m) => m,
         Err(why) => format!("<error {:?}>", why),
     };
 
     Ok(format!(
-        "Hello, I'm logged in as {}. RAM usage: proc {}, sys {}",
+        "Hello, I'm logged in as {}.\nProcess usage: {}\nSystem usage: {}",
         tag, proc_mem, sys_mem
     ))
 }
